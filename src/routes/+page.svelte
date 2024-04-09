12158947beb6b31ff918d3c577c76689dabfaa17
Stephon Fraser <programmer3@muneshwers.com>
2024-04-09 14:23:50 -0400

    Added header and styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Header from "./Header.svelte";
+    import './styles.css';
+</script>
+
+<Header />
+<p>Add content here</p>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,40 @@
+<script>
+    let signedin = false;
+</script>
+
+<header>
+    <div class="title">
+        <h1>Muneshwers Drivers Schedule</h1>
+    </div>
+    <div class="actions">
+        <button type="button" class="signin-button">
+            {signedin ? 'Sign Out' : 'Sign In'}
+        </button>
+    </div>
+</header>
+
+<style>
+    
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px;
+        font-weight: bold;
+    }
+
+    .signin-button {
+        padding: 10px;
+        border-radius: 5px;
+        border: 0;
+        background-color: #0047ab;
+        color: white;
+        font-size: medium;
+    }
+
+    .signin-button:hover {
+        cursor: pointer;
+        background-color: #003580;
+        transition: 0.5s;
+    }
+</style>
