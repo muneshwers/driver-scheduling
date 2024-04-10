b28126728a01e236669c5afe1d6efffbf6e88da6
Stephon Fraser <programmer3@muneshwers.com>
2024-04-10 09:28:21 -0400

    Created signin page

---
+++ src/routes/signin/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    import '../styles.css';
+    let isLoggedIn;
+</script>
+
+<div class="signin-page">
+    <div class="container">
+        <div class="title">
+            <h1>Muneshwers Driver Schedule</h1>
+        </div>
+        <form action="POST">
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button type="submit" class="signin-submit">Sign In</button>
+        </form>
+    </div>
+    
+</div>
+
+<style>
+    input {
+        padding: 10px;
+        border: 1px rgb(82, 82, 82) solid;
+        outline: none;
+        border-radius: 10px;
+        width: 80%;
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: 5%;
+    }
+
+    input:hover, input:focus {
+        border: 1px #0047ab solid;
+    }
+</style>
