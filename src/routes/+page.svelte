00d5de58214ffb494083ca642882cde96396b66f
Stephon Fraser <programmer3@muneshwers.com>
2024-04-10 11:56:08 -0400

    Added data from data.js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,19 @@
 <script>
     import { onMount } from 'svelte';
     import { Calendar } from "@fullcalendar/core";
+    import { drivers, events } from "./data";
     import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
     import './styles.css';
 
     let calendarEl;
     let isLoggedIn = true;
     let calendar;
+    let eventData = events;
+
+    $: driverInput = '';
+    $: fromInput = '';
+    $: toInput = '';
+    $: dateInput = '';
 
     onMount(() => {
         calendar = new Calendar(calendarEl, {
@@ -16,36 +23,23 @@
                 today: 'Today' // Change the text for the "Today" button here
             },
             height: "100%",
-            width: "100%",
-            resources: [
-                { id: 'a', title: 'Johnson Daniels'},
-                { id: 'b', title: 'Christian Murray'},
-                { id: 'c', title: 'Mash Burnedead'},
-                { id: 'd', title: 'Midoriya Izuku'},
-                { id: 'e', title: 'Brad English'},
-                { id: 'f', title: 'James Harrison'},
-                { id: 'g', title: 'Quincy Monroe'},
-                { id: 'h', title: 'Bartholomew Ew'},
-                { id: 'i', title: 'Ewart British'},
-                { id: 'j', title: 'Halle Lujah'},
-            ],
-            events: [
-                {
-                    title: 'Driving',
-                    start: '2024-04-09T10:00:00',
-                    end: '2024-04-09T12:00:00',
-                    resourceId: 'a'
-                },
-                {
-                    title: 'Driving',
-                    start: '2024-04-09T13:00:00',
-                    end: '2024-04-09T15:00:00',
-                    resourceId: 'b'
-                }
-            ]
+            resources: drivers,
+            events: eventData
         })
         calendar.render();
-    })
+    });
+
+    const handleSubmit = (event) => {
+        let eventDetails = {
+            title: "Driving",
+            start: dateInput+"T"+fromInput+":00",
+            end: dateInput+"T"+toInput+":00",
+            resourceId: driverInput
+        }
+        eventData.push(eventDetails);
+        eventData = eventData;
+        console.log("Schedule updated!", eventDetails, ". Events are now: ", eventData);
+    }
 
     
 
@@ -75,17 +69,20 @@
     </div>
 
     {#if isLoggedIn}
-        <form action="POST" class="formContainer">
+        <form action="" class="formContainer">
             <h1>Schedule Driver</h1>
-            <select name="drivers">
+            <select name="drivers" bind:value={driverInput}>
                 <option value="Select Driver">Select Driver:</option>
-                <option value="Johnson Daniels">Johnson Daniels</option>
-                <option value="Christian Murray">Christian Murray</option>
+                <option value="1">Johnson Barrymore</option>
+                <option value="2">Christian Murray</option>
+                <option value="3">Izuku Midoriya</option>
+                <option value="4">Brad English</option>
             </select>
-            <input type="time" placeholder="From" />
-            <input type="time" placeholder="To" />
-            <input type="date" placeholder="Date" />
-            <button type="submit" class="submit-button">Schedule</button>
+            <input type="time" placeholder="From" bind:value={fromInput}/>
+            <input type="time" placeholder="To" bind:value={toInput}/>
+            <input type="date" placeholder="Date" bind:value={dateInput}/>
+            <button type="submit" class="submit-button" on:click={() => handleSubmit()}>Schedule</button>
+            <span>{driverInput} from {fromInput} to {toInput} on {dateInput}.</span>
         </form>
     {/if}
 </div>
---
+++ src/routes/data.js
@@ -0,0 +1,50 @@
+export const users = [
+    {
+        username: "stephon",
+        password: "hi123"
+    }
+];
+
+export const drivers = [
+    {
+        id: '1',
+        title: 'Johnson Barrymore'
+    },
+    {
+        id: '2',
+        title: 'Christian Murray'
+    },
+    {
+        id: '3',
+        title: 'Izuku Midoriya'
+    },
+    {
+        id: '4',
+        title: 'Brad English'
+    },
+    {
+        id: '5',
+        title: 'Halle Lujah'
+    },
+];
+
+export let events = [
+    {
+        title: 'Driving',
+        start: '2024-04-10T10:00:00',
+        end: '2024-04-10T11:00:00',
+        resourceId: '1'
+    },
+    {
+        title: 'Driving',
+        start: '2024-04-10T11:30:00',
+        end: '2024-04-10T12:00:00',
+        resourceId: '2'
+    },
+    {
+        title: 'Driving',
+        start: '2024-04-10T09:00:00',
+        end: '2024-04-10T09:30:00',
+        resourceId: '3'
+    },
+];
