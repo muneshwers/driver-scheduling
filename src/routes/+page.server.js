ad0802a4ad7a519c664447f9f222c89541160034
Stephon Fraser <programmer3@muneshwers.com>
2024-04-12 14:49:29 -0400

    Added eventsAllCols for all columns in table

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -14,6 +14,7 @@ import { supabase } from "$lib/supabaseClient";
         end: singleEvent.end,
         resourceId: singleEvent.resourceId
       })),
+      eventsAllCols: events ?? [],
       driverList: drivers.map((driver) => ({
         id: driver.id,
         title: driver.name
