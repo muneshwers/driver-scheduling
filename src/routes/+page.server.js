86467130e71b8d4b36f4e38c5a7458f90f057117
Stephon Fraser <programmer3@muneshwers.com>
2024-04-12 11:10:11 -0400

    Loaded data from supabase into page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,8 +1,24 @@
-import { drivers, events } from "./data";
+import { supabase } from "$lib/supabaseClient";
+import { eventsRecords } from "../stores/eventStore";
 
-export function load() {
-    return {
-        driverList: drivers,
-        eventsList: events
+  export async function load() {
+    const { data: drivers, error: driverError } = await supabase.from("drivers").select();
+    const { data: events, error: eventError } = await supabase.from("events").select();
+    if(driverError || eventError) {
+        return console.log(driverError || eventError);
     }
-}
+    eventsRecords.set(events);
+    return {
+      drivers: drivers ?? [],
+      eventsList: events.map((singleEvent) => ({
+        title: singleEvent.title,
+        start: singleEvent.start,
+        end: singleEvent.end,
+        resourceId: singleEvent.resourceId
+      })),
+      driverList: drivers.map((driver) => ({
+        id: driver.id,
+        title: driver.name
+      }))
+    };
+  }
