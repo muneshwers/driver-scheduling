3697405baa0396098ee778c516f9aa343e3fd85b
Stephon Fraser <programmer3@muneshwers.com>
2024-04-12 11:17:36 -0400

    Used supabase data to validate user login

--- src/routes/signin/+page.server.js
+++ src/routes/signin/+page.server.js
@@ -1,9 +1,13 @@
-import { users } from '../data.js';
+// import { users } from '../data.js';
 import { redirect } from '@sveltejs/kit';
+import { supabase } from "$lib/supabaseClient";
 
 
 export const actions = {
     default: async ({request}) => {
+        const { data: users, error } = await supabase.from("users").select();
+        if(error) return console.log(error);
+        console.log(users);
         const data = await request.formData();
         const username = data.get('username');
         const password = data.get('password');
