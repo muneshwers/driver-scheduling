a1d59fa4816a320a21ae18d5d2db13ffc4c58260
Stephon Fraser <programmer3@muneshwers.com>
2024-04-20 12:43:45 -0400

    Created form validations for empty inputs and incorrect credentials

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,9 +1,36 @@
 <script>
+    import { page } from '$app/stores';
     import '../styles.css';
 
+    //Check for errors from params
+    let errorSignin = "";
+    const url = new URL($page.url);
+    const searchParams = url.searchParams;
+    errorSignin = searchParams.get('errorSignin');
+
+    //Inputs
     $: username = '';
     $: password = '';
 
+    //Toggles and Messages
+    let errorToggle = false;
+    let errorMessage = '';
+    let buttonToggle = true;
+
+    if (errorSignin == "IncorrectCredentials") {
+        errorToggle = true;
+        errorMessage = "Incorrect username and password. Please try again.";
+        
+    }
+
+    //Form input validation
+    const checkInputs = () => {
+        if(!username || !password) {
+            buttonToggle = true;
+            return
+        }
+        buttonToggle = false;
+    }
 </script>
 <svelte:head>
 	<title>Sign In - Driver Scheduling</title>
@@ -14,49 +41,19 @@
         <div class="title">
             <h1>Muneshwers Driver Schedule</h1>
         </div>
+        {#if errorToggle}
+            <div class="error-message">{errorMessage}</div>
+        {/if}
         <form method="post">
-            <input type="text" placeholder="Username" bind:value={username} name="username"/>
-            <input type="password" placeholder="Password" bind:value={password} name="password"/>
-            <button type="submit" class="signin-submit" >Sign In</button>
+            <input type="text" placeholder="Username" bind:value={username} name="username" class="signin-input {errorToggle == true ? 'input-error' : 'default-input'}" on:input={() => checkInputs()}/>
+            <input type="password" placeholder="Password" bind:value={password} name="password" class="signin-input {errorToggle == true ? 'input-error' : 'default-input'}" on:input={() => checkInputs()}/>
+            <button type="submit" class="signin-submit" disabled={buttonToggle}>Sign In</button>
         </form>
         <br />
         <div class="backTohome">
-            <a href="/">Back to Home</a>
+            <a href="/" class="remove-link-defaults">Back to Home</a>
         </div>
         
     </div>
     
 </div>
-
-<style>
-    input {
-        padding: 10px;
-        border: 1px rgb(82, 82, 82) solid;
-        outline: none;
-        border-radius: 10px;
-        width: 80%;
-        margin-left: auto;
-        margin-right: auto;
-        margin-bottom: 5%;
-    }
-
-    input:hover, input:focus {
-        border: 1px #0047ab solid;
-    }
-
-    a{
-        text-decoration: none;
-        color: black;
-    }
-    
-    a:hover {
-        color: #0047ab;
-        transition: 0.5s;
-        text-decoration: underline;
-    }
-
-    .backTohome{
-        padding: 10px;
-    }
-
-</style>
