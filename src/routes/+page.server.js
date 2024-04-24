6db469e47258a88f9ff6e81d62238d8d8abd5fa8
Stephon Fraser <programmer3@muneshwers.com>
2024-04-24 14:58:20 -0400

    Remove driver data source from page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,18 +1,7 @@
-import { supabase } from "$lib/supabaseClient";
-
 
   export async function load({cookies}) {
     const access = cookies.get("loginCredents");
-    const { data: drivers, error: driverError } = await supabase.from("drivers").select();
-    if(driverError) {
-        return console.log(driverError);
-    }
     return {
-      access: access,
-      drivers: drivers ?? [],
-      driverList: drivers.map((driver) => ({
-        id: driver.id,
-        title: driver.name
-      }))
+      access: access
     };
   }
