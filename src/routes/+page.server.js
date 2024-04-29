9e77b8ad58dbdc6bc66f0738844046f681ea2c01
Stephon Fraser <programmer3@muneshwers.com>
2024-04-29 09:13:43 -0400

    Added logic to log user session to public_sessions and pass user data to site

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,35 @@
+import { supabase } from "$lib/supabaseClient";
+import { redirect } from '@sveltejs/kit';
 
   export async function load({cookies}) {
+    //Get cookies
     const access = cookies.get("loginCredents");
+    const userSession = cookies.get("sessionId");
+
+    //Pulls session for session ID
+    const { data: sessions, error } = await supabase.from("public_sessions").select();
+    const { data: users, error: usersError } = await supabase.from("users").select();
+    if(error) {
+      console.error("Unable to pull sessions: ", error);
+      throw redirect(302, "/signin");
+    }
+    if(usersError) {
+      console.error("Unable to pull users records: ", usersError);
+      throw redirect(302, "/signin");
+    }
+
+    const foundSession = sessions.find((session) => session.session_key == userSession);
+    if(!foundSession) {
+      console.error("Unable to find session using: ", userSession);
+      throw redirect(302, "/signin");
+    }
+    const loggedUser = users.find((user) => user.id == foundSession.user);
+    if(!loggedUser) {
+      console.error("Unable to find user from session id: ", foundSession.user);
+      throw redirect(302, "/signin");
+    }    
     return {
+      userInfo: loggedUser,
       access: access
     };
   }
