7b17cbdba677755826c436549344ffbae6e61bd7
Stephon Fraser <programmer3@muneshwers.com>
2024-05-09 12:20:41 -0400

    Configured notifications to only be sent to admins/schedulers and scheduled driver

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,1 +1,17 @@
 console.log("Service worker here")
+
+self.addEventListener('message', (event) => {
+  let notification = event.data;
+  let userSession = event.data.data.loggedUser;
+  let driverNotify = event.data.data.driverName;
+  let userType = event.data.data.userType;
+  console.log(`User logged in is ${userSession} as ${userType}. Driver notified is ${driverNotify}.`)
+  if (userSession == driverNotify || userType!="driver") {
+      self.registration.showNotification(
+      notification.title,
+      notification.options
+    ).catch((error) => {
+      console.log(error);
+    });
+  }
+});
