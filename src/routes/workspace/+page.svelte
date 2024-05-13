cd064c8479ade8d6ac57bdfe4e8cdad2e026439f
Stephon Fraser <programmer3@muneshwers.com>
2024-05-13 11:26:26 -0400

    Created test and workspace pages

---
+++ src/routes/workspace/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import mllogo from "../../mllogo.png";
+    import '../styles.css';
+    $: isLoggedIn = true;
+</script>
+
+<header>
+    <div class="title">
+        <img src={mllogo} alt="Muneshwers Limited Logo" class="ml-logo">
+        <div class="raleway-medium title-text"><span class="title-text-span">Muneshwers</span> Drivers Schedule</div>
+    </div>
+    <div class="actions">
+        <a href={'/signin'}>
+            <button type="button" class="signin-button">
+                {isLoggedIn ? 'Sign Out' : 'Sign In'}
+            </button>
+        </a>
+    </div>
+</header>
