fd81cd32a3d97b11757996753833f2b7d4c682b0
Stephon Fraser <programmer3@muneshwers.com>
2024-05-29 10:06:36 -0400

    Added push event to trigger notifications

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,6 +5,12 @@ self.addEventListener('message', (event) => {
   let userSession = event.data.data.loggedUser;
   let driverNotify = event.data.data.driverName;
   let userType = event.data.data.userType;
+  const options = {
+    icon: '../static/dispatch.png'
+  }
+
+  console.log(options);
+
   console.log(`User logged in is ${userSession} as ${userType}. Driver notified is ${driverNotify}.`)
   if (userSession == driverNotify) {
       self.registration.showNotification(
@@ -16,13 +22,64 @@ self.addEventListener('message', (event) => {
   }
 });
 
-self.addEventListener('sync', (event) => {
-  console.log("Sync event", event);
-  if (event.tag === 'syncTrips') {
-    event.waitUntil(sendTripNotif());
-  }
-})
+self.addEventListener('push', (event) => {
+  console.log('Received a push message', event.data.text());
 
-const sendTripNotif = () => {
+  const payload = JSON.parse(event.data.text()) ?? 'no payload';
 
-}
+  const title = payload.title;
+  const body = payload.body;
+  const icon = '/dispatch.png';
+  const tag = 'simple-push-demo-notification-tag';
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      icon: icon,
+      tag: tag
+    })
+  );
+
+  // event.waitUntil(resubscribeToPush());
+});
+
+
+// async function resubscribeToPush() {
+//   return self.registration.pushManager.getSubscription()
+//     .then(function(subscription) {
+//       if (subscription) {
+//         return subscription.unsubscribe();
+//       }
+//     })
+//     .then(async function() {
+//       const res = await fetch('/api/vapidPubKey');
+//       const { data } = await res.json();
+//       return self.registration.pushManager.subscribe({
+//         userVisibleOnly: true,
+//         applicationServerKey: data
+//       });
+//     })
+//     .then(function(subscription) {
+//       console.log('Resubscribed to push notifications:', subscription);
+//       sendSubscriptionToServer(subscription);
+//     })
+//     .catch(function(error) {
+//       console.error('Failed to resubscribe:', error);
+//     });
+// }
+
+//   const sendSubscriptionToServer = async (subscription) => {
+//       try {
+//           const res = await fetch('/api/addSubscription', {
+//               method: 'POST',
+//               headers: {
+//                   'Content-Type': 'application/json'
+//               },
+//               body: JSON.stringify({ subscription, userId: userInfo.id })
+//           });
+//           if(!res.ok) throw new Error(`Error saving subscription on server: ${res.statusText} (${res.status})`);
+//       } catch (error) {
+//           console.error("Error saving subscription on server: ", error);
+//           unsubscribe();
+//       }
+//   }
